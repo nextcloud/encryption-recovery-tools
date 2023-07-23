5dd6b172f33f28666120665dd86ea6df52b7c5ea
Yahe <hello@yahe.sh>
2023-07-23 22:51:39 +0200

    introduce tests for trashbin files
    Signed-off-by: Yahe <hello@yahe.sh>

---
+++ tests/src/server-side-encryption/trashbin_owncloud09.php
@@ -0,0 +1,12 @@
+<?php
+// use prepared test setup
+include_once(__DIR__."/main.php");
+
+final class trashbin_owncloud09 extends main
+{
+	const INSTANCEID        = "ocfpxvgrexon";
+	const RECOVERY_PASSWORD = "recovery";
+	const SECRET            = "wIvlyCohkHpW9+wLcibHffmPZdDzbOP9zEstv5nMCsYIuMlO";
+	const USER_PASSWORDS    = ["admin" => "admin"];
+	const VERSION           = "trashbin_09.0.0";
+}
