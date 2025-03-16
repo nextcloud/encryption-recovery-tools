00281541d3df5371a13eecd35fc336d4a6c66f44
Yahe <hello@yahe.sh>
2025-03-16 18:34:32 +0100

    add E2E improvements and Nextcloud31 test
    Signed-off-by: Yahe <hello@yahe.sh>

--- tests/src/end-to-end-encryption/version20_weak_sha1.php
+++ tests/src/end-to-end-encryption/version20_weak_sha1.php
@@ -2,9 +2,9 @@
 // use prepared test setup
 include_once(__DIR__."/main.php");
 
-final class version20 extends main {
+final class version20_weak_sha1 extends main {
 	const EXTERNAL_STORAGES = [];
 	const SOURCEPATHS       = [];
 	const USER_MNEMONICS    = ["admin" => "drift company glass demise table grass skill master oval wait century kite"];
-	const VERSION           = "version20";
+	const VERSION           = "version20_weak_sha1";
 }
