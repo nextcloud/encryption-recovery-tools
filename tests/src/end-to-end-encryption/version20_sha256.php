877c16b0da8b4ed4de48855ec1b5532a0313af12
Yahe <hello@yahe.sh>
2025-03-16 18:25:43 +0100

    add E2E improvements and Nextcloud31 test
    Signed-off-by: Yahe <hello@yahe.sh>

---
+++ tests/src/end-to-end-encryption/version20_sha256.php
@@ -0,0 +1,10 @@
+<?php
+// use prepared test setup
+include_once(__DIR__."/main.php");
+
+final class version20_sha256 extends main {
+	const EXTERNAL_STORAGES = [];
+	const SOURCEPATHS       = [];
+	const USER_MNEMONICS    = ["admin" => "busy equip guard poverty music cabin promote label payment uncle arena toy"];
+	const VERSION           = "version20_sha256";
+}
